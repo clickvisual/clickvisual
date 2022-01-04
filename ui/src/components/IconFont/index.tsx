337ce6b2817b06cdc8838d98a0b712d76db01fd2
maomin <maomin@shimo.im>
2022-01-04 17:51:59 +0800

    fix: update icon url

--- ui/src/components/IconFont/index.tsx
+++ ui/src/components/IconFont/index.tsx
@@ -1,6 +1,6 @@
-import { createFromIconfontCN } from '@ant-design/icons';
+import { createFromIconfontCN } from "@ant-design/icons";
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_3023111_13hbd7ec8rkm.js',
+  scriptUrl: "//at.alicdn.com/t/font_3023111_u84rpy14b4o.js",
 });
 
 export default IconFont;
