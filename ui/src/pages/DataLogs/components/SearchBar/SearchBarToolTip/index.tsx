9b91ddb5ec6998c3aae3f1204724d2b0dd2b32e5
m1666 <160663534@qq.com>
2022-01-11 15:24:36 +0800

    fix: Optimization hints

--- ui/src/pages/DataLogs/components/SearchBar/SearchBarToolTip/index.tsx
+++ ui/src/pages/DataLogs/components/SearchBar/SearchBarToolTip/index.tsx
@@ -1,11 +1,11 @@
-import searchBarStyles from '@/pages/DataLogs/components/SearchBar/index.less';
+import searchBarStyles from "@/pages/DataLogs/components/SearchBar/index.less";
 
 const SearchBarToolTip = () => {
   return (
     <div>
       <div className={searchBarStyles.tooltipTitle}>
         <span>查询：</span>
-        <span>1. 指定字段查询：Method=Get and Status=200</span>
+        <span>1. 指定字段查询：Method='Get' and Status='200'</span>
       </div>
       <div className={searchBarStyles.tooltipTitle}></div>
     </div>
