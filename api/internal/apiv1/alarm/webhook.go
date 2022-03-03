ef6f670f8a3e428c21d771fff5c7348dd0cc87fe
kl7sn <mex7.0828@gmail.com>
2022-03-03 18:54:03 +0800

    fix: webhook api
    Signed-off-by: kl7sn <mex7.0828@gmail.com>

---
+++ api/internal/apiv1/alarm/webhook.go
@@ -0,0 +1,31 @@
+package alarm
+
+import (
+	"time"
+
+	"github.com/shimohq/mogo/api/pkg/component/core"
+)
+
+type Alert struct {
+	Labels      map[string]string `json:"labels"`
+	Annotations map[string]string `json:"annotations"`
+	StartsAt    time.Time         `json:"startsAt"`
+	EndsAt      time.Time         `json:"endsAt"`
+}
+
+type Notification struct {
+	Version           string            `json:"version"`
+	GroupKey          string            `json:"groupKey"`
+	Status            string            `json:"status"`
+	Receiver          string            `json:"receiver"`
+	GroupLabels       map[string]string `json:"groupLabels"`
+	CommonLabels      map[string]string `json:"commonLabels"`
+	CommonAnnotations map[string]string `json:"commonAnnotations"`
+	ExternalURL       string            `json:"externalURL"`
+	Alerts            []Alert           `json:"alerts"`
+}
+
+func Webhook(c *core.Context) {
+	c.JSONOK()
+	return
+}
