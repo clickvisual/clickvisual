6f6f9e8d72b83403baab259853785a373effedc0
xiongzhi <729715624@qq.com>
2022-05-05 15:03:28 +0800

    feat: The hash index of parameters is added

--- ui/src/models/datalogs/types.ts
+++ ui/src/models/datalogs/types.ts
@@ -28,6 +28,11 @@ export type PaneType = {
   highCharts: HighChartsResponse | undefined;
 };
 
+export enum hashType {
+  siphash = 1,
+  urlhash = 2,
+}
+
 export interface Extra {
   isPaging?: boolean; // 是否是切换页面
   reqParams?: QueryParams; // 请求参数
