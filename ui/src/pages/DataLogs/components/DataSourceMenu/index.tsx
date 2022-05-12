8c1c130c238116745e0be151002cdef09af44c35
m1666 <160663534@qq.com>
2022-05-12 15:29:41 +0800

    fix: The log library is not displayed when the database is empty

--- ui/src/pages/DataLogs/components/DataSourceMenu/index.tsx
+++ ui/src/pages/DataLogs/components/DataSourceMenu/index.tsx
@@ -1,16 +1,37 @@
 import dataSourceMenuStyles from "@/pages/DataLogs/components/DataSourceMenu/index.less";
 import SourceHeader from "@/pages/DataLogs/components/DataSourceMenu/SourceHeader";
 import LoggingLibrary from "@/pages/DataLogs/components/DataSourceMenu/LoggingLibrary";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useModel } from "@@/plugin-model/useModel";
 import classNames from "classnames";
+import { Empty } from "antd";
+import { useIntl } from "umi";
 
 const DataSourceMenu = () => {
-  const { doGetDatabaseList } = useModel("dataLogs");
+  const { doGetDatabaseList, currentDatabase } = useModel("dataLogs");
   const { foldingState } = useModel("dataLogs");
+
+  const i18n = useIntl();
+
   useEffect(() => {
     doGetDatabaseList();
   }, []);
+
+  const LogLibrary = useMemo(() => {
+    if (!currentDatabase) {
+      return (
+        <Empty
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          style={{ marginBottom: 10 }}
+          description={i18n.formatMessage({
+            id: "alarm.rules.selected.placeholder.database",
+          })}
+        />
+      );
+    }
+
+    return <LoggingLibrary />;
+  }, [currentDatabase]);
 
   return (
     <div
@@ -20,7 +41,7 @@ const DataSourceMenu = () => {
       )}
     >
       <SourceHeader />
-      <LoggingLibrary />
+      {LogLibrary}
     </div>
   );
 };
