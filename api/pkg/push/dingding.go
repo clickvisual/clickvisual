9115429deedb73dce6d53ad83acd1193f7290dba
MEX7 <mex7.0828@gmail.com>
2022-05-20 11:51:55 +0800

    refactor: alert channel factory

--- api/pkg/push/dingding.go
+++ api/pkg/push/dingding.go
@@ -1,4 +1,4 @@
-package alertmanager
+package push
 
 import (
 	"bytes"
---
+++ api/pkg/push/imp.go
@@ -0,0 +1,30 @@
+package push
+
+import (
+	"errors"
+
+	"github.com/clickvisual/clickvisual/api/pkg/model/db"
+	"github.com/clickvisual/clickvisual/api/pkg/model/view"
+)
+
+const (
+	ChannelDingDing int = 1
+	ChannelWeChat   int = 2
+)
+
+type Operator interface {
+	Send(notification view.Notification, alarm *db.Alarm, channel *db.AlarmChannel) (err error)
+}
+
+func Instance(typ int) (Operator, error) {
+	var err error
+	switch typ {
+	case ChannelDingDing:
+		return &DingDing{}, nil
+	case ChannelWeChat:
+		return &WeChat{}, nil
+	default:
+		err = errors.New("undefined channels")
+	}
+	return nil, err
+}
