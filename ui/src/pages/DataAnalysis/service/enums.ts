11ca9af9778f665d322b2c44d743bb470f93b060
m1666 <160663534@qq.com>
2022-06-15 15:12:31 +0800

    fix(data analysis): add enums

--- ui/src/pages/DataAnalysis/service/enums.ts
+++ ui/src/pages/DataAnalysis/service/enums.ts
@@ -10,6 +10,48 @@ export enum bigDataNavEnum {
 }
 
 export enum folderType {
-  "node" = 1,
-  "folder" = 2,
+  node = 1,
+  folder = 2,
+}
+
+export enum PrimaryEnums {
+  /**
+   * 数据开发
+   */
+  mining = 1,
+
+  /**
+   * 临时查询
+   */
+  short = 3,
+}
+
+export enum SecondaryEnums {
+  /**
+   * 数据库
+   */
+  database = 1,
+  /**
+   * 数据集成
+   */
+  dataIntegration = 2,
+}
+
+export enum TertiaryEnums {
+  /**
+   * clickhouse
+   */
+  clickhouse = 10,
+  /**
+   * mysql
+   */
+  mysql = 11,
+  /**
+   * 离线分析
+   */
+  offline = 20,
+  /**
+   * 实时分析
+   */
+  realtime = 21,
 }
