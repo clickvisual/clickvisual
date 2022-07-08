979bb729894468a2b5359e295b488f2e78ac77ea
MEX7 <mex7.0828@gmail.com>
2022-07-08 16:49:37 +0800

    feat: Node remote run API
    Signed-off-by: MEX7 <mex7.0828@gmail.com>

--- api/internal/service/node.go
+++ api/internal/service/node.go
@@ -7,26 +7,53 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/clickvisual/clickvisual/api/internal/invoker"
+	"github.com/clickvisual/clickvisual/api/internal/service/bigdata/node"
 	"github.com/clickvisual/clickvisual/api/pkg/model/db"
+	"github.com/clickvisual/clickvisual/api/pkg/model/view"
 )
 
+func NodeRun(nodeId, uid int) (res view.RespRunNode, err error) {
+	n, err := db.NodeInfo(invoker.Db, nodeId)
+	if err != nil {
+		return
+	}
+	if n.LockUid != uid {
+		err = errors.New("please get the node lock and try again")
+		return
+	}
+	nc, err := db.NodeContentInfo(invoker.Db, n.ID)
+	if err != nil {
+		return
+	}
+	res, err = node.Operator(&n, &nc, node.OperatorRun)
+	if err != nil {
+		return
+	}
+	afterNodeInfo, err := db.NodeInfo(invoker.Db, nodeId)
+	if err != nil {
+		return
+	}
+	res.Status = afterNodeInfo.Status
+	return
+}
+
 func NodeTryLock(uid, configId int) (err error) {
-	var node db.BigdataNode
+	var n db.BigdataNode
 
 	tx := invoker.Db.Begin()
 	{
-		err = tx.Set("gorm:query_option", "FOR UPDATE").Where("id = ?", configId).First(&node).Error
+		err = tx.Set("gorm:query_option", "FOR UPDATE").Where("id = ?", configId).First(&n).Error
 		if err != nil {
 			tx.Rollback()
 			return fmt.Errorf("configuration does not exist")
 		}
 
-		if node.LockUid != 0 && node.LockUid != uid {
+		if n.LockUid != 0 && n.LockUid != uid {
 			tx.Rollback()
 			return fmt.Errorf("failed to release the edit lock because another client is currently editing")
 		}
 
-		err = tx.Model(&db.BigdataNode{}).Where("id = ?", node.ID).Updates(map[string]interface{}{
+		err = tx.Model(&db.BigdataNode{}).Where("id = ?", n.ID).Updates(map[string]interface{}{
 			"lock_at":  time.Now().Unix(),
 			"lock_uid": uid,
 		}).Error
@@ -39,22 +66,22 @@ func NodeTryLock(uid, configId int) (err error) {
 }
 
 func NodeUnlock(uid, configId int) (err error) {
-	var node db.BigdataNode
+	var n db.BigdataNode
 
 	tx := invoker.Db.Begin()
 	{
-		err = tx.Set("gorm:query_option", "FOR UPDATE").Where("id = ?", configId).First(&node).Error
+		err = tx.Set("gorm:query_option", "FOR UPDATE").Where("id = ?", configId).First(&n).Error
 		if err != nil {
 			tx.Rollback()
 			return fmt.Errorf("configuration does not exist")
 		}
 
-		if node.LockUid != 0 && node.LockUid != uid {
+		if n.LockUid != 0 && n.LockUid != uid {
 			tx.Rollback()
 			return fmt.Errorf("failed to release the edit lock because another client is currently editing")
 		}
 
-		err = tx.Model(&db.BigdataNode{}).Where("id = ?", node.ID).Updates(map[string]interface{}{
+		err = tx.Model(&db.BigdataNode{}).Where("id = ?", n.ID).Updates(map[string]interface{}{
 			"lock_at":  nil,
 			"lock_uid": 0,
 		}).Error
