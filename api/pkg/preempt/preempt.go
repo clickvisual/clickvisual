1e141d2ebc24fabf90c88707afcbe342b68086d0
MEX7 <mex7.0828@gmail.com>
2022-08-04 19:20:53 +0800

    feat: Permission module and scheduled task scheduling support multi-POD deployment

---
+++ api/pkg/preempt/preempt.go
@@ -0,0 +1,139 @@
+package preempt
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ego-component/eredis"
+	"github.com/gotomicro/cetus/pkg/knet"
+	"github.com/gotomicro/ego/core/elog"
+)
+
+const (
+	_lock = "preempt:lock:%s" // uid -> clients
+
+	retryInterval = time.Second * 30
+	lockTime      = time.Minute * 3
+)
+
+func keyLock(key string) string {
+	if key == "" {
+		key = "default"
+	}
+	return fmt.Sprintf(_lock, key)
+}
+
+type Preempt struct {
+	ctx       context.Context
+	redis     *eredis.Component
+	startFunc func()
+	closeFunc func()
+	key       string
+	isOccupy  bool
+	ip        string
+}
+
+func NewPreempt(ctx context.Context, db *eredis.Component, key string, startFunc, closeFunc func()) *Preempt {
+	ipStr, _ := knet.Local()
+	p := &Preempt{
+		ctx:       ctx,
+		redis:     db,
+		startFunc: startFunc,
+		closeFunc: closeFunc,
+		isOccupy:  false,
+		key:       keyLock(key),
+		ip:        ipStr,
+	}
+	go p.sara()
+	return p
+}
+
+func (p *Preempt) Close() {
+	if !p.isOccupy {
+		// not worker, do nothing
+		return
+	}
+	p.closeFunc() // do closeFunc
+	if p.isStillOnlyWorker() {
+		p.unlock() // unlock the key
+	}
+}
+
+func (p *Preempt) sara() {
+	for {
+		p.flows()
+		time.Sleep(retryInterval)
+	}
+}
+
+func (p *Preempt) flows() {
+	if p.isOccupy {
+		// func is processing, just renew the lock
+		p.renew()
+		elog.Info("preempt", elog.String("step", "renew"), elog.String("key", p.key))
+		return
+	}
+	// try to grab the lock, to be the worker
+	if p.lock() {
+		// lock success, do func please
+		elog.Info("preempt", elog.String("step", "startFunc"), elog.String("key", p.key))
+		p.isOccupy = true
+		go p.startFunc()
+		return
+	}
+	// the competition failed, try to be the only worker in the next life cycle
+	return
+}
+
+func (p *Preempt) renew() {
+	_, err := p.redis.Client().Expire(p.ctx, p.key, lockTime).Result()
+	if err != nil {
+		elog.Error("preempt", elog.String("step", "renew"), elog.String("key", p.key), elog.String("error", err.Error()))
+		return
+	}
+	return
+}
+
+func (p *Preempt) isStillOnlyWorker() bool {
+	// check redis key is set locally
+	val, err := p.redis.Client().Get(p.ctx, p.key).Result()
+	if err != nil {
+		return false
+	}
+	if val == p.ip {
+		return true
+	}
+	return false
+}
+
+func (p *Preempt) lock() bool {
+	ok, err := p.redis.Client().SetNX(p.ctx, p.key, p.ip, lockTime).Result()
+	if err != nil {
+		elog.Error("preempt", elog.String("step", "lock"), elog.String("key", p.key), elog.String("error", err.Error()))
+		return false
+	}
+	elog.Debug("preempt", elog.String("step", "SetNX"), elog.Any("ok", ok))
+	if ok {
+		// set success
+		elog.Info("preempt", elog.String("step", "lockSucc"))
+		return true
+	}
+	// set failed
+	elog.Warn("preempt", elog.String("key", p.key), elog.String("step", "lockFailed"))
+	return false
+}
+
+func (p *Preempt) unlock() {
+	ok, err := p.redis.Client().Del(p.ctx, p.key).Result()
+	if err != nil {
+		elog.Error("preempt", elog.String("step", "unlock"), elog.String("key", p.key), elog.String("error", err.Error()))
+		return
+	}
+	if ok == 0 {
+		elog.Error("preempt", elog.String("key", p.key), elog.String("step", "deleteFailed"))
+		return
+	}
+	elog.Info("preempt", elog.String("key", p.key), elog.String("step", "deleteSucc"))
+	return
+}
---
+++ api/pkg/preempt/preempt_test.go
@@ -0,0 +1,81 @@
+package preempt
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/ego-component/eredis"
+	"github.com/gotomicro/ego/core/econf"
+
+	"github.com/clickvisual/clickvisual/api/internal/invoker"
+)
+
+func init() {
+	wd, _ := os.Getwd()
+	for !strings.HasSuffix(wd, "ws-api") {
+		wd = filepath.Dir(wd)
+	}
+	f, err := os.Open(wd + "/config/local.toml")
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = f.Close() }()
+	err = econf.LoadFromReader(f, toml.Unmarshal)
+	if err != nil {
+		panic(err)
+	}
+	_ = invoker.Init()
+}
+
+func TestNewPreempt(t *testing.T) {
+	type args struct {
+		ctx       context.Context
+		db        *eredis.Component
+		key       string
+		startFunc func()
+		closeFunc func()
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Preempt
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test-1",
+			args: args{
+				ctx: context.Background(),
+				db:  invoker.Redis,
+				key: "mogo",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for i := 0; i < 10; i++ {
+				sf := func() {
+					for {
+						time.Sleep(time.Second * 3)
+						fmt.Printf("i am working... id is %d\n", i)
+					}
+				}
+				ef := func() {
+					fmt.Printf("i will go to sleep... id is %d\n", i)
+				}
+				go func() {
+					p := NewPreempt(tt.args.ctx, tt.args.db, tt.args.key, sf, ef)
+					time.Sleep(time.Minute)
+					p.Close()
+				}()
+			}
+		})
+	}
+	time.Sleep(time.Minute + time.Second*10)
+}
