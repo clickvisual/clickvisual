b765d80f94f2a505fb86770c644818d34dc7087d
m1666 <160663534@qq.com>
2022-08-22 14:21:23 +0800

    feat(log): Removes scrolling hidden histograms

--- ui/src/pages/DataLogs/components/QueryResult/Content/RawLog/RawLogList/index.tsx
+++ ui/src/pages/DataLogs/components/QueryResult/Content/RawLog/RawLogList/index.tsx
@@ -1,13 +1,11 @@
 import rawLogListStyles from "@/pages/DataLogs/components/QueryResult/Content/RawLog/RawLogList/index.less";
 import LogItem from "@/pages/DataLogs/components/QueryResult/Content/RawLog/RawLogList/LogItem";
 import { useModel } from "@@/plugin-model/useModel";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import classNames from "classnames";
-import useLogListScroll from "@/pages/DataLogs/hooks/useLogListScroll";
 
 const RawLogList = () => {
-  const { currentLogLibrary, onChangeHiddenHighChart, logs, logPanesHelper } =
-    useModel("dataLogs");
+  const { currentLogLibrary, logs, logPanesHelper } = useModel("dataLogs");
   const { logPanes } = logPanesHelper;
 
   const oldPane = useMemo(() => {
@@ -15,21 +13,9 @@ const RawLogList = () => {
     return logPanes[currentLogLibrary?.id.toString()];
   }, [currentLogLibrary?.id, logPanes]);
 
-  const containerProps = useLogListScroll();
-
-  useEffect(() => {
-    if (containerProps.ref.current && oldPane?.histogramChecked) {
-      containerProps.ref.current.scrollTop = 0;
-      onChangeHiddenHighChart(false);
-    }
-  }, [logs]);
-
   const list = logs?.logs || [];
   return (
-    <div
-      className={classNames(rawLogListStyles.rawLogListMain)}
-      {...containerProps}
-    >
+    <div className={classNames(rawLogListStyles.rawLogListMain)}>
       {list.map((logItem: any, index: number) => (
         <LogItem
           foldingChecked={oldPane?.foldingChecked}
