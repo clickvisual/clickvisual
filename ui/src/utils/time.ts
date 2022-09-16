a6f37dd56749bc13d50300d9f160a81da2f01bd0
xiongzhi <729715624@qq.com>
2022-09-16 18:43:59 +0800

    fix: The link-time conversion function is compatible with UTC0 format

--- ui/src/utils/time.ts
+++ ui/src/utils/time.ts
@@ -2,16 +2,22 @@ import moment from "moment";
 
 /**
  * 微秒级别时间格式转时间戳
- * @param time string "2022-09-02T02:31:49.006385319Z"
+ * @param time string "2022-09-02T02:31:49.006385319Z" or "2022-09-16T03:42:00.124Z"
  * @returns  number 1662085909006385
  */
 export const microsecondTimeStamp: (time: string) => number = (
   time: string
 ) => {
-  return parseInt(
-    moment(time.split(".")[0] + "Z").valueOf() / 1000 +
-      time.split(".")[1].substring(0, 6)
-  );
+  console.log(moment("2022-09-16T03:42:00.124Z").format("x"), "9090");
+  if (time?.length == 30) {
+    return parseInt(
+      moment(time.split(".")[0] + "Z").valueOf() / 1000 +
+        time.split(".")[1].substring(0, 6)
+    );
+  } else if (time?.length == 24) {
+    return parseInt(moment("2022-09-16T03:42:00.124Z").format("x")) * 1000;
+  }
+  return 0;
 };
 
 /**
