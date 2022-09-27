6d31315a7daa21c867cca9e7e6a15023ff157cec
MEX7 <mex7.0828@gmail.com>
2022-09-27 10:36:14 +0800

    fix(config): Only watch configmap

--- api/internal/service/kube/api/types.go
+++ api/internal/service/kube/api/types.go
@@ -1,9 +1,7 @@
 package api
 
 import (
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
@@ -50,17 +48,17 @@ type GroupVersionResourceKind struct {
 
 // 这里做k8s资源的映射, 应对后续k8s版本更新时的Version,Group等变动
 var KindToResourceMap = map[string]ResourceMap{
-	ResourceNameEvent: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNameEvent,
-			},
-			Kind: KindNameEvent,
-		},
-		Namespaced: true,
-	},
+	// ResourceNameEvent: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameEvent,
+	// 		},
+	// 		Kind: KindNameEvent,
+	// 	},
+	// 	Namespaced: true,
+	// },
 	ResourceNameConfigMap: {
 		GroupVersionResourceKind: GroupVersionResourceKind{
 			GroupVersionResource: schema.GroupVersionResource{
@@ -72,94 +70,94 @@ var KindToResourceMap = map[string]ResourceMap{
 		},
 		Namespaced: true,
 	},
-	ResourceNameDeployment: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    appsv1.GroupName,
-				Version:  appsv1.SchemeGroupVersion.Version,
-				Resource: ResourceNameDeployment,
-			},
-			Kind: KindNameDeployment,
-		},
-		Namespaced: true,
-	},
-	ResourceNameIngress: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    extensionsv1beta1.GroupName,
-				Version:  extensionsv1beta1.SchemeGroupVersion.Version,
-				Resource: ResourceNameIngress,
-			},
-			Kind: KindNameIngress,
-		},
-		Namespaced: true,
-	},
-	ResourceNameNamespace: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNameNamespace,
-			},
-			Kind: KindNameNamespace,
-		},
-		Namespaced: false,
-	},
-	ResourceNameNode: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNameNode,
-			},
-			Kind: KindNameNode,
-		},
-		Namespaced: false,
-	},
-	ResourceNamePod: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNamePod,
-			},
-			Kind: KindNamePod,
-		},
-		Namespaced: true,
-	},
-	ResourceNameReplicaSet: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				//Group:    extensionsv1beta1.GroupName,
-				Group: appsv1.GroupName,
-				//Version:  extensionsv1beta1.SchemeGroupVersion.Version,
-				Version:  appsv1.SchemeGroupVersion.Version,
-				Resource: ResourceNameReplicaSet,
-			},
-			Kind: KindNameReplicaSet,
-		},
-		Namespaced: true,
-	},
-	ResourceNameService: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNameService,
-			},
-			Kind: KindNameService,
-		},
-		Namespaced: true,
-	},
-	ResourceNameEndpoint: {
-		GroupVersionResourceKind: GroupVersionResourceKind{
-			GroupVersionResource: schema.GroupVersionResource{
-				Group:    corev1.GroupName,
-				Version:  corev1.SchemeGroupVersion.Version,
-				Resource: ResourceNameEndpoint,
-			},
-			Kind: KindNameEndpoint,
-		},
-		Namespaced: true,
-	},
+	// ResourceNameDeployment: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    appsv1.GroupName,
+	// 			Version:  appsv1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameDeployment,
+	// 		},
+	// 		Kind: KindNameDeployment,
+	// 	},
+	// 	Namespaced: true,
+	// },
+	// ResourceNameIngress: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    extensionsv1beta1.GroupName,
+	// 			Version:  extensionsv1beta1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameIngress,
+	// 		},
+	// 		Kind: KindNameIngress,
+	// 	},
+	// 	Namespaced: true,
+	// },
+	// ResourceNameNamespace: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameNamespace,
+	// 		},
+	// 		Kind: KindNameNamespace,
+	// 	},
+	// 	Namespaced: false,
+	// },
+	// ResourceNameNode: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameNode,
+	// 		},
+	// 		Kind: KindNameNode,
+	// 	},
+	// 	Namespaced: false,
+	// },
+	// ResourceNamePod: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNamePod,
+	// 		},
+	// 		Kind: KindNamePod,
+	// 	},
+	// 	Namespaced: true,
+	// },
+	// ResourceNameReplicaSet: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			//Group:    extensionsv1beta1.GroupName,
+	// 			Group: appsv1.GroupName,
+	// 			//Version:  extensionsv1beta1.SchemeGroupVersion.Version,
+	// 			Version:  appsv1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameReplicaSet,
+	// 		},
+	// 		Kind: KindNameReplicaSet,
+	// 	},
+	// 	Namespaced: true,
+	// },
+	// ResourceNameService: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameService,
+	// 		},
+	// 		Kind: KindNameService,
+	// 	},
+	// 	Namespaced: true,
+	// },
+	// ResourceNameEndpoint: {
+	// 	GroupVersionResourceKind: GroupVersionResourceKind{
+	// 		GroupVersionResource: schema.GroupVersionResource{
+	// 			Group:    corev1.GroupName,
+	// 			Version:  corev1.SchemeGroupVersion.Version,
+	// 			Resource: ResourceNameEndpoint,
+	// 		},
+	// 		Kind: KindNameEndpoint,
+	// 	},
+	// 	Namespaced: true,
+	// },
 }
