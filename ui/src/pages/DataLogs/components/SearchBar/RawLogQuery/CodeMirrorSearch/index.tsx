e0871016c9a466adf9892482a1db24755b1c99a9
xiongzhi <729715624@qq.com>
2022-10-11 18:07:27 +0800

    style: codemirror input box style changes

--- ui/src/pages/DataLogs/components/SearchBar/RawLogQuery/CodeMirrorSearch/index.tsx
+++ ui/src/pages/DataLogs/components/SearchBar/RawLogQuery/CodeMirrorSearch/index.tsx
@@ -60,52 +60,59 @@ const Editors = (props: {
 
   return (
     <div className={styles.editors} key={title + "editors"}>
-      <CodeMirror
-        className={styles.editorsDom}
-        ref={formRefs}
-        key={title}
-        editorDidMount={onEditorDidMount}
-        onKeyPress={() => {
-          // 按键的时候触发代码提示
-          formRefs.current.editor.showHint();
-        }}
-        onChange={(CodeMirror: string, changeObj: any, value: string) =>
-          onChange(value)
-        }
-        value={sqlValue}
-        options={{
-          // 显示行号
-          lineNumbers: true,
-          // 改变行号文案
-          lineNumberFormatter: () => "WHERE",
-          mode: {
-            name: "text/x-mysql",
-          },
-          // 自定义快捷键
-          extraKeys: { Enter: handleEnter },
-          hintOptions: {
-            // 自定义提示选项
-            completeSingle: false, // 当匹配只有一项的时候是否自动补全
-            // 自定义的提示库
-            tables: tables,
-          },
-          autofocus: false,
-          styleActiveLine: true,
-          // 主题
-          theme: "neo",
-          // 溢出滚动而非换行
-          lineWrapping: false,
-          foldGutter: true,
-          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-          lint: false,
-          indentUnit: 2,
-          // 光标高度
-          cursorHeight: 1,
-          placeholder: placeholder || "",
-          // tab缩进
-          tabSize: 2,
-        }}
-      />
+      <span className={styles.where}>WHERE</span>
+      <div className={styles.codemirrorInput}>
+        <CodeMirror
+          className={styles.editorsDom}
+          ref={formRefs}
+          key={title}
+          editorDidMount={onEditorDidMount}
+          onKeyPress={() => {
+            // 按键的时候触发代码提示
+            formRefs.current.editor.showHint();
+          }}
+          onChange={(CodeMirror: string, changeObj: any, value: string) =>
+            onChange(value)
+          }
+          value={sqlValue}
+          options={{
+            // 显示行号
+            lineNumbers: false,
+            // 改变行号文案
+            lineNumberFormatter: () => "WHERE",
+            mode: {
+              name: "text/x-mysql",
+            },
+            // 自定义快捷键
+            extraKeys: { Enter: handleEnter },
+            hintOptions: {
+              // 自定义提示选项
+              completeSingle: false, // 当匹配只有一项的时候是否自动补全
+              // 自定义的提示库
+              tables: tables,
+            },
+            autofocus: false,
+            styleActiveLine: true,
+            // 主题
+            theme: "neo",
+            // 溢出滚动而非换行
+            lineWrapping: false,
+            foldGutter: true,
+            // gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+            gutters: false,
+            lint: false,
+            indentUnit: 2,
+            // 光标高度
+            cursorHeight: 1,
+            placeholder: placeholder || "",
+            // tab缩进
+            tabSize: 2,
+            // 滚动条样式
+            scrollbarStyle: null,
+          }}
+        />
+        <span className={styles.afterBox}></span>
+      </div>
     </div>
   );
 };
