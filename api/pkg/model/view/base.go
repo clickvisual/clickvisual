19e53fad820f60b8805afd9dcaa43a49dae65b52
MEX7 <mex7.0828@gmail.com>
2022-11-11 17:19:04 +0800

    fix: Filter struct

--- api/pkg/model/view/base.go
+++ api/pkg/model/view/base.go
@@ -45,21 +45,21 @@ type IndexItem struct {
 
 type (
 	ReqQuery struct {
-		Tid           int                   `json:"tid" form:"tid"`
-		Database      string                `form:"database"`
-		Table         string                `form:"table"`
-		DatabaseTable string                `form:"databaseTable"`
-		Field         string                `form:"field"`
-		Query         string                `form:"query"`
-		TimeField     string                `form:"timeField"`
-		TimeFieldType int                   `form:"timeFieldType"`
-		ST            int64                 `form:"st"`
-		ET            int64                 `form:"et"`
-		Page          uint32                `form:"page"`
-		PageSize      uint32                `form:"pageSize"`
-		AlarmMode     int                   `form:"alarmMode"`
-		Filters       []db.ReqUpdateCollect `form:"filters"`
-		GroupByCond   string                `form:"groupByCond"`
+		Tid           int      `json:"tid" form:"tid"`
+		Database      string   `form:"database"`
+		Table         string   `form:"table"`
+		DatabaseTable string   `form:"databaseTable"`
+		Field         string   `form:"field"`
+		Query         string   `form:"query"`
+		TimeField     string   `form:"timeField"`
+		TimeFieldType int      `form:"timeFieldType"`
+		ST            int64    `form:"st"`
+		ET            int64    `form:"et"`
+		Page          uint32   `form:"page"`
+		PageSize      uint32   `form:"pageSize"`
+		AlarmMode     int      `form:"alarmMode"`
+		Filters       []string `form:"filters"`
+		GroupByCond   string   `form:"groupByCond"`
 	}
 
 	RespQuery struct {
